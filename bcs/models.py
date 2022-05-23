6268914a6c910729d514f279a98f21fdfd810d16
apunguray <apungura@gmail.com>
2022-05-23 08:50:41 +0800

    commets

--- bcs/models.py
+++ bcs/models.py
@@ -42,13 +42,14 @@ VALID_SKILL = (
     ("Project Manager", "Project Manager")
 )
 
-
+                                                                                                   
+# CREATE DATABASE TABLE OR MODEL FOR SKILLS
 class Skill(models.Model):
     skill_id = models.AutoField(primary_key=True)
     skill = models.CharField(max_length=200, choices=VALID_SKILL)
     description = models.TextField(blank=True, null=True)
     rate_of_pay = models.CharField(max_length=100)
-
+                                                                                                  
     def employees(self):
         employees_with_skill = Employee.objects.filter(skill__skill=self.skill)
         employees_with_skill_list = []
@@ -56,18 +57,19 @@ class Skill(models.Model):
             employees_with_skill_list.append(
                 employee.first_name + " " + employee.last_name)
         return ', '.join(employees_with_skill_list)
-
+                                                                                                  
     # Prevent duplicate skills
     def save(self, *args, **kwargs):
         if self.skill in [skill.skill for skill in Skill.objects.all()]:
             raise ValueError("Skill already exists")
         else:
             super(Skill, self).save(*args, **kwargs)
-
+                                                                                                  
     def __str__(self):
         return self.skill
+                                                                                                  
 
-
+# CREATE DATABASE TABLE OR MODEL FOR PROJECT
 class Project(models.Model):
     project_id = models.AutoField(primary_key=True)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
@@ -77,11 +79,12 @@ class Project(models.Model):
     start_date = models.DateField(auto_now_add=True)
     end_date = models.DateField(auto_created=True)
     actual_cost = models.CharField(max_length=1000, blank=True, null=True)
-
+                                                                                      
     def __str__(self):
         return f'PROJECT ID: {self.project_id}'
+                                                                                      
 
-
+# CREATE DATABASE TABLE OR MODEL FOR PROJECT
 class ProjectSchedule(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     task_id = models.AutoField(primary_key=True)
@@ -90,10 +93,10 @@ class ProjectSchedule(models.Model):
     number_of_employees = models.IntegerField()
     start_date = models.DateField(auto_now_add=True)
     end_date = models.DateField(auto_created=True)
-
+                                                                                      
     def skills_required(self):
         return ", ".join([str(s) for s in self.skills.all()])
-
+                                                                                      
     # Only assign a project schedule to an employee if the employee is not already assigned to a project schedule
     def assign_employee(self, employee):
         if employee.project_schedule is None:
@@ -101,12 +104,12 @@ class ProjectSchedule(models.Model):
             employee.save()
         else:
             raise ValueError("Employee already assigned to a project schedule")
-
+                                                                                      
     def __str__(self):
-        return f'{self.description}'
+        return f'{self.description}'                                                                                      
 
 
-# Employee model \\ table
+# CREATE DATABASE TABLE OR MODEL FOR EMPLOYEE
 class Employee(models.Model):
     EmployeeId = models.AutoField(primary_key=True)
     skill = models.ManyToManyField(Skill)
@@ -115,14 +118,15 @@ class Employee(models.Model):
     middle_initial = models.CharField(max_length=5)
     date_of_hire = models.DateTimeField(auto_now_add=True)
     region = models.CharField(max_length=50, choices=VALID_REGIONS, default=0)
-
+                                                                                      
     def skills(self):
         return ", ".join([str(s) for s in self.skill.all()])
-
+                                                                                      
     def __str__(self):
         return f'{self.first_name} {self.middle_initial}'
 
 
+# CREATE DATABASE TABLE OR MODEL FOR PROJECT ASSIGNMENT
 class ProjectAssignment(models.Model):
     assignment_id = models.AutoField(primary_key=True)
     employee = models.ManyToManyField(Employee, blank=True, null=True)
@@ -130,33 +134,34 @@ class ProjectAssignment(models.Model):
         ProjectSchedule, on_delete=models.CASCADE)
     start_date = models.DateField(auto_now_add=True)
     end_date = models.DateField(auto_created=True)
-    
+                                                                                      
     # get all skills from project_schedule_task
     def skills (self):
         return ", ".join([str(s) for s in self.project_schedule_task.skills.all()])
-    
+                                                                                      
     # Get project task from project_schedule_task
     def project_task(self):
         return self.project_schedule_task.description
-
+                                                                                      
     # get project_schedule_task start date
     def scheduled_start_date(self):
         return self.project_schedule_task.start_date
-
+                                                                                      
     # get project_schedule_task end date
     def scheduled_end_date(self):
         return self.project_schedule_task.end_date
-
+                                                                                      
     def assginment_start_date(self):
         return self.start_date
-
+                                                                                      
+    # get end date
     def assginment_end_date(self):
         return self.end_date
-
+                                                                                      
     # get employees
     def employees(self):
         return ", ".join([str(e) for e in self.employee.all()])
-
+                                                                                      
     def __str__(self):
         return f'PROJECT ID: {self.project_schedule_task.task_id}'
 
@@ -168,7 +173,7 @@ class WorkLog(models.Model):
         ProjectAssignment, on_delete=models.CASCADE)
     total_hours = models.IntegerField()
     bill_number = models.CharField(max_length=50, blank=True, null=True)
-
+                                                                                      
     def __str__(self) -> str:
         return f'{self.employee.first_name} {self.employee.middle_initial}'
 
@@ -177,6 +182,6 @@ class Bill(models.Model):
     bill_id = models.AutoField(primary_key=True)
     customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
     total_hours = models.IntegerField()
-
+                                                                                      
     def __str__(self):
         return f'BILL ID: {self.bill_id}'
